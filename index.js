cd35f99e961ab5d37276d08d527415be234b85c4
Pierre Arnaud <arnaud@epsitec.ch>
2015-05-10 07:59:04 +0200

    Add password component.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   Button: require ('./components/buttons/Button/Button.jsx'),
+  PasswordField: require ('./components/forms/fields/PasswordField/PasswordField.jsx'),
   TextField: require ('./components/forms/fields/TextField/TextField.jsx')
 };
