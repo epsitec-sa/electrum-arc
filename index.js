0841ac1efe2d7184ea74be6f6de8c72bd4271bc6
Pierre Arnaud <arnaud@epsitec.ch>
2015-05-11 06:01:20 +0200

    Fix require for Checkbox.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  Button: require ('./components/buttons/Button/Button.jsx'),
-  Checkbox: require ('./components/forms/options/Checkbox.jsx'),
+  Button:        require ('./components/buttons/Button/Button.jsx'),
+  Checkbox:      require ('./components/forms/options/Checkbox/Checkbox.jsx'),
   PasswordField: require ('./components/forms/fields/PasswordField/PasswordField.jsx'),
-  TextField: require ('./components/forms/fields/TextField/TextField.jsx')
+  TextField:     require ('./components/forms/fields/TextField/TextField.jsx')
 };
