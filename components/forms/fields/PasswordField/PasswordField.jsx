79d4e96c0524b22195de497fc29aa3ecff7b3725
Pierre Arnaud <arnaud@epsitec.ch>
2015-05-11 15:13:35 +0200

    Add change event handler to PasswordField.

--- components/forms/fields/PasswordField/PasswordField.jsx
+++ components/forms/fields/PasswordField/PasswordField.jsx
@@ -1,5 +1,7 @@
 'use strict';
 
+var eventHandlers = require ('../event-handlers.js');
+
 var React = require ('react');
 var E     = require ('e');
 
@@ -7,18 +9,22 @@ var E     = require ('e');
 
 module.exports = E.createClass({
 
+  handleChange: eventHandlers.handleChange,
+
   render: function () {
 
     var labelText = E.getText (this);
-    var placeholder = this.props.placeholder || '...';
+    var placeholder = this.props.placeholder;
     var style = E.getStyle (this, require ('./PasswordField.styles.js'));
     var value = E.getValue (this);
     var disabled = E.getState (this, s => s.disabled);
-    
+
     return (
       <label style={style.label}>
         {labelText}
-        <input style={style.input}
+        <input
+          onChange={this.handleChange}
+          style={style.input}
           type="password"
           id={this.props.id}
           placeholder={placeholder}
