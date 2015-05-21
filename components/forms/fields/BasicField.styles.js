afa6f2c69812025cb1623016475e1eba962958d7
Samuel Loup <loup@epsitec.ch>
2015-05-21 13:29:05 +0200

    Refine Basic field style

--- components/forms/fields/BasicField.styles.js
+++ components/forms/fields/BasicField.styles.js
@@ -9,8 +9,10 @@ module.exports = {
     fontSize: '18px',
     fontWeight: 600,
     paddingLeft: '.7em',
-    paddingRight: '2em',
+    paddingRight: '.7em',
     paddingTop: '.2em',
-    paddingBottom: '.2em'
+    paddingBottom: '.2em',
+    marginTop: '.1em',
+    marginBottom: '.1em'
   }
 };
