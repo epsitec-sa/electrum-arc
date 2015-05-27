1fc3addd7b202409083ceba35a204dd4c9d5fdbb
Samuel Loup <loup@epsitec.ch>
2015-05-27 09:59:32 +0200

    Remove magic injection
    I think we can't do magic here because
    we are using babel and jsx loader.
    We need to inject more tools for this kind of
    injection. It's possible but heavy risky and hard to debug.

--- index.js
+++ index.js
@@ -10,29 +10,7 @@ files.forEach (function (file) {
   var component     = __webpack_require__ (componentId); // jshint ignore:line
   var matches       = file.match (/([^\/\\]+)\.jsx$/);
   var componentType = matches[1];
-  component.webpackId = componentId;
   components[componentType] = component;
-});
-
-Object.keys (components).forEach (function (type) {
-  var component  = components[type];
-  var realRender = component.render;
-  component.render = function () {
-    console.log ('!INJECT ARC COMPONENTS!');
-    var injection = '';
-    var res       = null;
-    injection += React.justToMakeSureThisDoesNotGetOptimizedAway ||
-                 E.justToMakeSureThisDoesNotGetOptimizedAway ||
-                '';
-    Object.keys (components).forEach (function (type) {
-      var component = components[type];
-      injection += 'var ' + type + ' =  __webpack_require__ (' + component.webpackId + ');\n';
-    });
-    injection += 'var render = ' + realRender + ';\n';
-    injection += 'res    = render.call (this);\n';
-    eval (injection); // jshint ignore:line
-    return res;
-  };
 });
 
 Object.keys (components).forEach (function (type) {
