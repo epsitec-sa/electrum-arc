9bb98c4601782b282ddfc0f0a4ff1a1ef60deaac
Samuel Loup <loup@epsitec.ch>
2015-05-28 22:22:40 +0200

    Add Link kinds for Menu context

--- components/links/Link.styles.js
+++ components/links/Link.styles.js
@@ -8,5 +8,17 @@ module.exports = {
   },
   footer: {
     color: '#fff'
+  },
+  'menu-heading': {
+    color: '#777',
+    fontFamily: 'sans-serif',
+    fontWeight: 100,
+    fontSize: '2em'
+  },
+  'menu-item': {
+    color: '#777',
+    fontFamily: 'sans-serif',
+    fontWeight: 100,
+    fontSize: '1em'
   }
 };
