ecc4d1b59558cd76a580f9255dbbe993ee1293d7
Samuel Loup <loup@epsitec.ch>
2015-06-11 10:15:50 +0200

    GoogleFontHack apply theme font only

--- components/extras/GoogleFontHack.jsx
+++ components/extras/GoogleFontHack.jsx
@@ -8,8 +8,6 @@ var E     = require ('e');
 module.exports = {
 
   render: function () {
-
-    var font = this.props.name || 'Roboto';
     var ss    = document.getElementById ('googlefonthack');
 
     if (!ss) {
@@ -18,16 +16,9 @@ module.exports = {
       ss.type = 'text/css';
       ss.rel  = 'stylesheet';
       ss.href = 'http://fonts.googleapis.com/css?family=';
-      ss.href += font;
+      ss.href += E.typo.font.split (',')[0];
       document.getElementsByTagName ('head')[0].appendChild(ss);
-      E.typo.font = font;
       console.log ('@! injected googlefont');
-    } else {
-      if (E.typo.font !== font) {
-        ss.href = 'http://fonts.googleapis.com/css?family=';
-        ss.href += font;
-        console.log ('@! rewrite googlefont');
-      }
     }
 
     var style = {
