a8db574ca92d49141f848c11be4d8d10dda081d6
Samuel Loup <loup@epsitec.ch>
2015-06-15 10:41:35 +0200

    Implement link like buttons

--- components/links/Link.jsx
+++ components/links/Link.jsx
@@ -9,12 +9,23 @@ module.exports = {
 
   theme: require ('./Link.styles.js'),
 
+  handleClick: function () {
+    E.bus.dispatch (this, this.props.href || this.props.id);
+  },
+
   render: function () {
-    var style = E.getStyle (this);
+    var text     = E.getText (this);
+    var style    = E.getStyle (this);
+    var disabled = E.getState (this, s => s.disabled);
 
     return (
       <div style={this.props.boxstyle}>
-        <a href={this.props.href} style={style}>
+        <a
+          href={this.props.href}
+          style={style}
+          onClick={this.handleClick}
+          >
+          {text}
           {this.props.children}
         </a>
       </div>
