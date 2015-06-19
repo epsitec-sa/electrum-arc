62776ea5b34b23c4debf56c201d67dbbeae2d8c9
Samuel Loup <samuel.loup@gmail.com>
2015-06-19 16:08:14 +0200

    Display value

--- components/texts/Text.jsx
+++ components/texts/Text.jsx
@@ -12,12 +12,13 @@ module.exports = {
   render: function () {
     var text     = E.getText (this);
     var style    = E.getStyle (this);
+    var value    = E.getValue (this);
 
     return (
       <div style={this.props.boxstyle}>
         <p
           style={style}>
-          {text}
+          {value ? value : text}
           {this.props.children}
         </p>
       </div>
