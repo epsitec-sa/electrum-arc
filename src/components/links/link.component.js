a98e3a3a618e469054ea42d082b7dec1b01b651a
Pierre Arnaud <arnaud@epsitec.ch>
2016-02-12 14:39:51 +0100

    Fix href issue with <Link>.

--- src/components/links/link.component.js
+++ src/components/links/link.component.js
@@ -23,31 +23,25 @@ export default class Link extends React.Component {
     const {children, state, href} = this.props;
     const disabled = Action.isDisabled (state);
     const text = state.get ('text') || (children ? null : '<missing>');
+    const attr = {};
+
+    if (href) {
+      // Only add href attribute if href exists, or else React will produce
+      // an <a href="">...</a> element, which is clickable.
+      attr.href = href;
+    }
 
     if (disabled) {
-      return (
-        <div>
-          <a
-            style={this.styles.with ('disabled')}
-            disabled='disabled' >
-            {text}
-            {children}
-          </a>
-        </div>
-      );
+      attr.style = this.styles.with ('disabled');
+      attr.disabled = 'disabled';
     } else {
-      return (
-        <div>
-          <a
-            href={href}
-            style={this.styles}
-            onClick={this.onClick} >
-            {text}
-            {children}
-          </a>
-        </div>
-      );
+      attr.style = this.styles;
+      attr.onClick = this.onClick;
     }
+
+    return (
+      <a {...attr}>{text}{children}</a>
+    );
   }
 }
 
