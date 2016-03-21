ff34d4ad04e09a40a30c6840c8d74974c496be9f
Samuel Loup <loup@epsitec.ch>
2016-03-21 17:19:43 +0100

    Remove uncompatible selectablelist component

--- src/components/lists/selectable-list.component.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import {BasicList} from './basic-list.component.js';
-import {SelectableContainerEnhance as selectable} from 'material-ui/lib/hoc/selectable-enhance';
-
-/******************************************************************************/
-
-let SelectableList = selectable (BasicList);
-export default SelectableList;
