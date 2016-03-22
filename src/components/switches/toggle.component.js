d20c9c8b58176bf799df2eb7ec806865db9e0ea5
Daniel Roux <roux@epsitec.ch>
2016-03-22 19:04:25 +0100

    Wrapping of Toggle from material-ui.

---
+++ src/components/switches/toggle.component.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import React from 'react';
+import {Action} from 'electrum';
+import {Toggle as MUIToggle} from 'material-ui';
+/******************************************************************************/
+
+export default class Toggle extends React.Component {
+
+  constructor (props) {
+    super (props);
+  }
+
+  render () {
+    const {state} = this.props.state;
+    const disabled = Action.isDisabled (state);
+    return (
+      <MUIToggle
+        onTouchTap={this.onClick}
+        id={this.props.id}
+        disabled={disabled}
+        label={this.props.label || this.read ('label')}
+        labelPosition={this.props.labelPosition || this.read ('labelPosition')}
+        toggled={this.props.toggled || this.read ('toggled')}
+        {...this.props}
+        >
+        {this.props.children}
+      </MUIToggle>
+    );
+  }
+}
+
+/******************************************************************************/
