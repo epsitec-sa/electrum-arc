ab115333826d8d90f07dabd01f3691e97a75580f
Daniel Roux <roux@epsitec.ch>
2016-04-14 21:20:35 +0200

    Enhanced fonctionality of BasicField.

--- src/components/forms/fields/basic-field.component.js
+++ src/components/forms/fields/basic-field.component.js
@@ -26,32 +26,57 @@ export default class BasicField extends React.Component {
   }
 
   render () {
-    const {state} = this.props.state;
+    const {state, width} = this.props;
     const disabled = Action.isDisabled (state);
+    const inputWidth = width || state.get ('width');
 
-    var fieldStyle = {
-      border: '1px solid #888',
+    var boxStyle = {
+      display:         'table-cell',
+      textAlign:       'center',
+      verticalAlign:   'middle',
+      border:          '1px solid #888',
       backgroundColor: '#fff',
-      padding: '5px',
+      padding:         '0px',
+      margin:          '0px',
+    };
+    var fieldStyle = {
+      display:         'table-cell',
+      width:           inputWidth,
+      height:          '32px',
+      textAlign:       'left',
+      verticalAlign:   'middle',
+      border:          'none',
+      padding:         '10px',
+      margin:          '0px',
     };
 
     return (
-      <input
-        onChange={this.onChange}
-        onFocus={this.onFocus}
-        onKeyDown={this.onKeyDown}
-        onKeyUp={this.onKeyUp}
-        onSelect={this.onSelect}
-        disabled={disabled}
-        id={this.props.id}
-        maxLength={this.props.maxLength}
-        placeholder={this.props.hintText || this.read ('hintText')}
-        style={fieldStyle}
-        type={this.props.type || 'text'}
-        key='input'
-        value={this.props.value || this.read ('value')}
-        {...this.props}
-        />
+      <span>
+        <span
+          disabled={disabled}
+          id={this.props.id}
+          style={boxStyle}
+          {...this.props}
+          >
+          <input
+            onChange={this.onChange}
+            onFocus={this.onFocus}
+            onKeyDown={this.onKeyDown}
+            onKeyUp={this.onKeyUp}
+            onSelect={this.onSelect}
+            disabled={disabled}
+            id={this.props.id}
+            maxLength={this.props.maxLength}
+            placeholder={this.props.hintText || this.read ('hintText')}
+            size={this.props.size || 'size'}
+            style={fieldStyle}
+            type={this.props.type || 'text'}
+            key='input'
+            value={this.props.value || this.read ('value')}
+            {...this.props}
+            />
+        </span>
+      </span>
     );
   }
 }
