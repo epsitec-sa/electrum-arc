b2a1fb01754491031f744d41b0e6b21762a5c932
Daniel Roux <roux@epsitec.ch>
2016-04-15 14:51:08 +0200

    Create BasicContainer component.

---
+++ src/components/containers/basic-container.component.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import React from 'react';
+import {Action} from 'electrum';
+/******************************************************************************/
+
+export default class BasicContainer extends React.Component {
+
+  constructor (props) {
+    super (props);
+  }
+
+  render () {
+    const {state, width} = this.props;
+    const disabled = Action.isDisabled (state);
+    const inputWidth = width || state.get ('width');
+
+    var containerStyle = {
+      display:         'flex',
+      flexDirection:   'column',
+      justifyContent:  'flex-start',
+      alignItems:      'stretch',
+      width:           inputWidth,
+      padding:         20,
+    };
+
+    return (
+      <div
+        disabled={disabled}
+        id={this.props.id}
+        style={containerStyle}
+        {...this.props}
+        />
+    );
+  }
+}
+
+/******************************************************************************/
