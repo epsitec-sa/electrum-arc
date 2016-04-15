68baf361925e4486d4477e4fcc9462d6c9617398
Daniel Roux <roux@epsitec.ch>
2016-04-15 13:31:01 +0200

    Create BasicLabel component.

---
+++ src/components/forms/fields/basic-label.component.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React from 'react';
+import {Action} from 'electrum';
+/******************************************************************************/
+
+export default class BasicLabel extends React.Component {
+  render () {
+    const {state, text, grow} = this.props;
+    const disabled = Action.isDisabled (state);
+    var inputText = text || state.get ('text');
+    var inputGrow = grow || state.get ('grow');
+
+    var labelStyle = {
+      flexGrow:        inputGrow,
+    };
+
+    return (
+      <label
+        disabled={disabled}
+        style={labelStyle}
+        >
+        {inputText}
+      </label>
+    );
+  }
+}
+
+/******************************************************************************/
