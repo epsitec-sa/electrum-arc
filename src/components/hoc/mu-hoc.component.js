c854d52935d2cdd5ef2d666833e964afdb508e9b
Pierre Arnaud <arnaud@epsitec.ch>
2016-04-28 09:54:50 +0200

    Revert commit; works with material-ui 0.15.0.

--- src/components/hoc/mu-hoc.component.js
+++ src/components/hoc/mu-hoc.component.js
@@ -4,15 +4,11 @@ import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import * as Colors from 'material-ui/styles/colors';
-import ColorManipulator from 'material-ui/utils/color-manipulator';
+import {fade} from 'material-ui/utils/colorManipulator';
 import Spacing from 'material-ui/styles/spacing';
 import zIndex from 'material-ui/styles/zIndex';
 
 /******************************************************************************/
-
-// Note: use ColorManipulator.fade with the full name for the tests to work.
-// Otherwise, the import of {fade} alone won't be able to find _decomposeColor
-// while running in Wallaby.js
 
 const theme = {
   spacing: Spacing,
@@ -29,7 +25,7 @@ const theme = {
     alternateTextColor: Colors.white,
     canvasColor: Colors.red,
     borderColor: Colors.grey300,
-    disabledColor: ColorManipulator.fade (Colors.darkBlack, 0.3),
+    disabledColor: fade (Colors.darkBlack, 0.3),
     pickerHeaderColor: Colors.cyan500,
   }
 };
