e6d094c3db8b73c859804aabdd01a7db3be71035
Daniel Roux <roux@epsitec.ch>
2016-08-29 15:09:06 +0200

    Add dash line to ticket.

--- src/components/containers/ticket.styles.js
+++ src/components/containers/ticket.styles.js
@@ -17,7 +17,21 @@ function lineTo (result, dx, dy) {
 // Arc to relative position.
 function arcTo (result, r, cx, cy) {
   // rx ry x-axis-rotation large-arc-flag sweep-flag x y
+  // see http://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
   result += 'a ' + r + ' '  + r + ' 0 0 0 ' + ' ' + cx + ' ' + cy + ' ';
+  return result;
+}
+
+function horizontalDash (result, r, len, dx) {
+  const step = parseInt (dx / len);
+  const over = (dx - (len * step)) / 2;
+  result = lineTo (result, over + r, 0);
+  let i = 0;
+  for (i = 0; i < step - 1; i++) {
+    result = lineTo (result, len - r - r, 0);
+    result = arcTo (result, r, r + r, 0);
+  }
+  result = lineTo (result, len - r + over, 0);
   return result;
 }
 
@@ -51,16 +65,19 @@ export default function styles (theme, props) {
   };
 
   const r = 10;
+  const rr = r * 0.3;
   const w = width.replace (/px/g, '');
   const h = height.replace (/px/g, '');
   let path = '';
   path = moveTo (path, 0, r);
   path = arcTo (path, r, r, -r);
-  path = lineTo (path, w - r - r, 0);
+  // path = lineTo (path, w - r - r, 0);
+  path = horizontalDash (path, rr, rr * 3, w - r - r);
   path = arcTo (path, r, r, r);
   path = lineTo (path, 0, h - r - r);
   path = arcTo (path, r, -r, r);
-  path = lineTo (path, -(w - r - r), 0);
+  // path = lineTo (path, -(w - r - r), 0);
+  path = horizontalDash (path, -rr, -rr * 3, -(w - r - r));
   path = arcTo (path, r, -r, -r);
 
   const svgStyle = {
