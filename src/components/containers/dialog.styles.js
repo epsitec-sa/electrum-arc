980615a174535155abc46230596ce4e1bfe82536
Daniel Roux <roux@epsitec.ch>
2016-09-05 09:02:00 +0200

    Put some Dialog values to theme.

--- src/components/containers/dialog.styles.js
+++ src/components/containers/dialog.styles.js
@@ -10,22 +10,27 @@ export default function styles (theme, props) {
   const inputHeight           = props.height;
   const inputKind             = props.kind;
 
-  let width             = inputWidth;
-  let height            = inputHeight;
+  let width           = inputWidth;
+  let height          = inputHeight;
+  let padding         = theme.shapes.dialogPadding;
+  let backgroundColor = theme.palette.dialogBackground;
+  let boxShadow       = theme.shapes.dialogShadow;
 
   const boxStyle = {
-    width:             width,
-    height:            height,
-    display:           'flex',
-    flexDirection:     'row',
-    backgroundColor:   '#f00',
-    zIndex:            '2',
-    position:          'absolute',
-    left:              '0px',
-    right:             '0px',
-    top:               '0px',
-    bottom:            '0px',
-    margin:            'auto',
+    width:           width,
+    height:          height,
+    display:         'flex',
+    flexDirection:   'row',
+    backgroundColor: backgroundColor,
+    boxShadow:       boxShadow,
+    zIndex:          '2',
+    position:        'absolute',
+    left:            '0px',
+    right:           '0px',
+    top:             '0px',
+    bottom:          '0px',
+    margin:          'auto',
+    padding:         padding,
   };
 
   return {
