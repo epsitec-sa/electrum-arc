8cf5f3b9ddde2cab09eff6da272a86e6da567ccb
Daniel Roux <roux@epsitec.ch>
2016-09-27 14:15:45 +0200

    Split data for trip-box component.

--- src/components/polypheme/trip-box.component.js
+++ src/components/polypheme/trip-box.component.js
@@ -41,13 +41,15 @@ export default class TripBox extends React.Component {
   }
 
   render () {
-    const width  = this.read ('width');
-    const height = this.read ('height');
-    const data   = this.read ('data');
+    const width    = this.read ('width');
+    const height   = this.read ('height');
+    const selected = this.read ('selected');
+    const color    = this.read ('color');
+    const data     = this.read ('data');
 
     const h = height ? height : '70px';
 
-    if (!data || typeof data.pickTime === 'undefined' || typeof data.dropTime === 'undefined') {
+    if (!data || typeof data.pick === 'undefined' || typeof data.drop === 'undefined') {
       return (
         <Container kind='thin-main' width={width} height={h} grow='1' {...this.link ()} >
         </Container>
@@ -57,43 +59,43 @@ export default class TripBox extends React.Component {
       const dimmedSize  = '75%';
 
       return (
-        <Container kind='thin-main' width={width} height={h} grow='1' selected={data.selected} color={data.color} {...this.link ()} >
+        <Container kind='thin-main' width={width} height={h} grow='1' selected={selected} color={color} {...this.link ()} >
           <Button kind='thin-left' glyph='arrows-alt' width='24px' {...this.link ()} />
           <Container kind='thin-column' border='right' grow='4' {...this.link ()} >
             <Container kind='thin-row' border='bottom' grow='1' {...this.link ()} >
               <Container kind='thin-row' grow='1' {...this.link ()} >
-                <Label text={data.pickTime} font-weight='bold' {...this.link ()} />
+                <Label text={data.pick.time} font-weight='bold' {...this.link ()} />
               </Container>
               <Container kind='thin-row' width='20px' {...this.link ()} >
                 <Label glyph='upload' glyph-color={dimmedColor} {...this.link ()} />
               </Container>
               <Container kind='thin-row' grow='3' {...this.link ()} >
-                <Label text={data.pickDesc} wrap='no' {...this.link ()} />
+                <Label text={data.pick.desc} wrap='no' {...this.link ()} />
               </Container>
               <Container kind='thin-row' grow='1' {...this.link ()} >
-                <Label text={data.pickZone} text-transform='uppercase' wrap='no' font-size={dimmedSize} {...this.link ()} />
+                <Label text={data.pick.zone} text-transform='uppercase' wrap='no' font-size={dimmedSize} {...this.link ()} />
               </Container>
               <Container kind='thin-row' grow='1' {...this.link ()} >
                 <Label grow='1' {...this.link ()} />
-                {this.getGlyphs (data.pickGlyphs)}
+                {this.getGlyphs (data.pick.glyphs)}
               </Container>
             </Container>
             <Container kind='thin-row' grow='1' {...this.link ()} >
               <Container kind='thin-row' grow='1' {...this.link ()} >
-                <Label text={data.dropTime} font-weight='bold' {...this.link ()} />
+                <Label text={data.drop.time} font-weight='bold' {...this.link ()} />
               </Container>
               <Container kind='thin-row' width='20px' {...this.link ()} >
                 <Label glyph='download' glyph-color={dimmedColor} {...this.link ()} />
               </Container>
               <Container kind='thin-row' grow='3' {...this.link ()} >
-                <Label text={data.dropDesc} wrap='no' {...this.link ()} />
+                <Label text={data.drop.desc} wrap='no' {...this.link ()} />
               </Container>
               <Container kind='thin-row' grow='1' {...this.link ()} >
-                <Label text={data.dropZone} text-transform='uppercase' wrap='no' font-size={dimmedSize} {...this.link ()} />
+                <Label text={data.drop.zone} text-transform='uppercase' wrap='no' font-size={dimmedSize} {...this.link ()} />
               </Container>
               <Container kind='thin-row' grow='1' {...this.link ()} >
                 <Label grow='1' {...this.link ()} />
-                {this.getGlyphs (data.dropGlyphs)}
+                {this.getGlyphs (data.drop.glyphs)}
               </Container>
             </Container>
           </Container>
