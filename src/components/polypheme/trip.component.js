f85233596796ce450be97529de47a54dd5d7ac16
Daniel Roux <roux@epsitec.ch>
2016-11-29 13:17:59 +0100

    Remove unused code.

--- src/components/polypheme/trip.component.js
+++ src/components/polypheme/trip.component.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {TripBox, TripTicket, TripTickets} from '../../all-components.js';
 
 /******************************************************************************/
@@ -10,36 +9,6 @@ export default class Trip extends React.Component {
 
   constructor (props) {
     super (props);
-  }
-
-  componentDidMount () {
-    const data = this.read ('data');
-    const ticketId = data.ticketId;
-    const tripId   = data.tripId;
-    if (ticketId && tripId) {
-      if (!window.document.trips) {
-        window.document.trips = {};
-      }
-      if (!window.document.trips[tripId]) {
-        window.document.trips[tripId] = new Map ();
-      }
-      window.document.trips[tripId].set (ticketId, this);
-    }
-  }
-
-  componentWillUnmount () {
-    const data = this.read ('data');
-    const ticketId = data.ticketId;
-    const tripId   = data.tripId;
-    if (ticketId && tripId) {
-      if (!window.document.trips) {
-        throw new Error (`Fatal error during Trip.componentWillUnmount with tripId=${tripId} (#1)`);
-      }
-      if (!window.document.trips[tripId]) {
-        throw new Error (`Fatal error during Trip.componentWillUnmount tripId=${tripId} (#2)`);
-      }
-      window.document.trips[tripId].delete (ticketId);
-    }
   }
 
   render () {
