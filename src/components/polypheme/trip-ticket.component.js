6e5b62495e8812d87600a6f0646e0f5857d7ecf7
Daniel Roux <roux@epsitec.ch>
2016-12-16 16:20:19 +0100

    Accept 'hh:mm:ss' into PlanedTime.

--- src/components/polypheme/trip-ticket.component.js
+++ src/components/polypheme/trip-ticket.component.js
@@ -61,6 +61,11 @@ export default class TripTicket extends React.Component {
       let h = time.substring (11, 13);
       let m = time.substring (14, 16);
       time = h + ':' + m;
+    } else if (time && time.length === 8) {
+      // If format '12:45:30', extract 'hh:mm'.
+      let h = time.substring (0, 2);
+      let m = time.substring (3, 5);
+      time = h + ':' + m;
     }
     return time;
   }
