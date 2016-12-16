47d0c6326c63d61721448c828ae241b357d6b842
Daniel Roux <roux@epsitec.ch>
2016-12-16 16:01:41 +0100

    Accept data without messenger.

--- src/components/polypheme/messenger-ticket.component.js
+++ src/components/polypheme/messenger-ticket.component.js
@@ -17,18 +17,25 @@ export default class MessengerTicket extends React.Component {
     const height = this.props.theme.shapes.messengerHeight;
     const data   = this.read ('data');
 
+    const photo = (data.Messenger && data.Messenger.Photo && data.Messenger.Photo.Glyph) ?
+      data.Messenger.Photo.Glyph :
+      'user';
+    const name = (data.Messenger && data.Messenger.Name) ?
+      data.Messenger.Name :
+      'A définir';
+
     return (
       <Ticket kind='ticket' shape='header' width={width} height={height} color='selected'
         drag-handle='messengers' no-drag='false' cursor='ew-resize'
         data={data} {...this.link ()} >
         <Container kind='column' grow='2' {...this.link ()} >
-          <Button glyph={data.Messenger.Photo.Glyph} kind='identity' {...this.link ()} />
+          <Button glyph={photo} kind='identity' {...this.link ()} />
         </Container>
         <Container kind='column' grow='1' {...this.link ()} >
           <Label glyph={data.Transport} glyph-size='150%' {...this.link ()} />
         </Container>
         <Container kind='column' grow='3' {...this.link ()} >
-          <Label text={data.Messenger.Name} text-color='#fff' {...this.link ()} />
+          <Label text={name} text-color='#fff' {...this.link ()} />
           <Label text={data.Revenue} font-weight='bold' text-color='#fff' {...this.link ()} />
         </Container>
       </Ticket>
