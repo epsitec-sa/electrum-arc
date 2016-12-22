51fc3af3ccd4f1dc725e61d1459abd592163a6c8
Daniel Roux <roux@epsitec.ch>
2016-12-22 16:38:02 +0100

    TripModify is ok in readonly mode.

--- src/components/polypheme/trip-modify.component.js
+++ src/components/polypheme/trip-modify.component.js
@@ -1,7 +1,20 @@
 'use strict';
 
 import React from 'react';
-import {Container, Button, Label, DialogModal} from '../../all-components.js';
+import {DialogModal, Container, Button, Label, LabelTextField, Separator} from '../../all-components.js';
+import {ColorHelpers} from 'electrum-theme';
+
+/******************************************************************************/
+
+function getTime (time) {
+  if (time && time.length === 33) {
+    // If format '2016-11-30T17:45:03.9052723+01:00', extract 'hh:mm'.
+    let h = time.substring (11, 13);
+    let m = time.substring (14, 16);
+    time = h + ':' + m;
+  }
+  return time;
+}
 
 /******************************************************************************/
 
@@ -26,29 +39,76 @@ export default class TripModify extends React.Component {
     this.closeModify ();
   }
 
-  render () {
+  renderHalf (ticket, type) {
+    let glyph, color, title, pd;
+    if (type.startsWith ('pick')) {
+      glyph = 'plus-square';
+      color = ColorHelpers.GetMarkColor (this.props.theme, 'pick');
+      title = 'Pick';
+      pd    = ticket.Trip.Pick;
+    } else {
+      glyph = 'minus-square';
+      color = ColorHelpers.GetMarkColor (this.props.theme, 'drop');
+      title = 'Drop';
+      pd    = ticket.Trip.Drop;
+    }
+
     return (
-      <DialogModal width='500px' height='400px' {...this.link ()}>
-        <Container kind='views' {...this.link ()} >
-          <Container kind='full-view' {...this.link ()} >
+      <Container kind='panes' {...this.link ()} >
+        <Label glyph={glyph} glyph-color={color} text={title} grow='1' kind='title' {...this.link ()} />
+        <Separator kind='space' {...this.link ()} />
+        <LabelTextField label-glyph='clock-o' hint-text='Heure'
+          value={getTime (pd.PlanedTime)} width='100px' {...this.link ()} />
+        <LabelTextField label-glyph='tag' hint-text='Description courte'
+          value={pd.ShortDescription} grow='1' {...this.link ()} />
+        <LabelTextField label-glyph='tags' hint-text='Description complète'
+          value={pd.LongDescription} grow='1' {...this.link ()} />
+      </Container>
+    );
+  }
 
-            <Container kind='pane-navigator' {...this.link ()} >
-              <Label text='Liste des mandats' grow='1' kind='title' {...this.link ()} />
-            </Container>
+  renderFooter () {
+    return (
+      <Container kind='actions' subkind='no-shadow' {...this.link ()} >
+        <Button action={() => this.modifyAccept ()} glyph='check' text='Modifier' kind='action'
+          grow='1' place='left' {...this.link ()} />
+        <Button action={() => this.modifyCancel ()} glyph='close' text='Annuler' kind='action'
+          grow='1' place='right' {...this.link ()} />
+      </Container>
+    );
+  }
 
-            <Container kind='panes' subkind='top-margin' {...this.link ()} >
-            </Container>
+  render () {
+    const ticket = this.read ('ticket');
 
-            <Container kind='actions' subkind='no-shadow' {...this.link ()} >
-              <Button action={() => this.modifyAccept ()} glyph='check' text='Modifier' kind='action'
-                grow='1' place='left' {...this.link ()} />
-              <Button action={() => this.modifyCancel ()} glyph='close' text='Annuler' kind='action'
-                grow='1' place='right' {...this.link ()} />
+    if (ticket.Type === 'both') {
+      return (
+        <DialogModal width='500px' height='500px' {...this.link ()}>
+          <Container kind='views' {...this.link ()} >
+            <Container kind='full-view' {...this.link ()} >
+
+              {this.renderHalf (ticket, 'pick')}
+              {this.renderHalf (ticket, 'drop')}
+              {this.renderFooter ()}
+
             </Container>
           </Container>
-        </Container>
-      </DialogModal>
-    );
+        </DialogModal>
+      );
+    } else {
+      return (
+        <DialogModal width='500px' height='300px' {...this.link ()}>
+          <Container kind='views' {...this.link ()} >
+            <Container kind='full-view' {...this.link ()} >
+
+              {this.renderHalf (ticket, ticket.Type)}
+              {this.renderFooter ()}
+
+            </Container>
+          </Container>
+        </DialogModal>
+      );
+    }
   }
 }
 
