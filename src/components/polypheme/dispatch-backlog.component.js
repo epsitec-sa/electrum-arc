edbef244594353762e7f4fc32fb11c293e3431cd
Daniel Roux <roux@epsitec.ch>
2016-12-22 13:33:31 +0100

    Dispatch-backlog has sort and filter fonctionnals.

--- src/components/polypheme/dispatch-backlog.component.js
+++ src/components/polypheme/dispatch-backlog.component.js
@@ -9,6 +9,65 @@ import {
   Trip
 } from '../../all-components.js';
 
+function getTime (time) {
+  if (time && time.length === 33) {
+    // If format '2016-11-30T17:45:03.9052723+01:00', extract 'hh:mm'.
+    let h = time.substring (11, 13);
+    let m = time.substring (14, 16);
+    time = h + ':' + m;
+  } else if (time && time.length === 8) {
+    // If format '12:45:30', extract 'hh:mm'.
+    let h = time.substring (0, 2);
+    let m = time.substring (3, 5);
+    time = h + ':' + m;
+  }
+  return time;
+}
+
+function sortTimePick (a, b) {
+  const ta = getTime (a.Trip.Pick.PlanedTime);
+  const tb = getTime (b.Trip.Pick.PlanedTime);
+  return ta.localeCompare (tb);
+}
+
+function sortTimeDrop (a, b) {
+  const ta = getTime (a.Trip.Drop.PlanedTime);
+  const tb = getTime (b.Trip.Drop.PlanedTime);
+  return ta.localeCompare (tb);
+}
+
+function sortZonePick (a, b) {
+  const ta = getTime (a.Trip.Pick.Zone);
+  const tb = getTime (b.Trip.Pick.Zone);
+  return ta.localeCompare (tb);
+}
+
+function sortZoneDrop (a, b) {
+  const ta = getTime (a.Trip.Drop.Zone);
+  const tb = getTime (b.Trip.Drop.Zone);
+  return ta.localeCompare (tb);
+}
+
+function sortProduct (a, b) {
+  const ta = getTime (a.Trip.Product);
+  const tb = getTime (b.Trip.Product);
+  return ta.localeCompare (tb);
+}
+
+function sortPrice (a, b) {
+  const ta = getTime (a.Trip.Price);
+  const tb = getTime (b.Trip.Price);
+  return tb.localeCompare (ta);
+}
+
+function sortWeight (a, b) {
+  const ta = getTime (a.Trip.Weight);
+  const tb = getTime (b.Trip.Weight);
+  return tb.localeCompare (ta);
+}
+
+/******************************************************************************/
+
 export default class DispatchBacklog extends React.Component {
 
   constructor (props) {
@@ -59,8 +118,13 @@ export default class DispatchBacklog extends React.Component {
 
   getSortList (data) {
     return [
-      this.getSortItem (data, 'Chronologique'),
-      this.getSortItem (data, 'Par types'),
+      this.getSortItem (data, 'Par heure pick', 'clock-o'),
+      this.getSortItem (data, 'Par heure drop', 'clock-o'),
+      this.getSortItem (data, 'Par zone pick',  'map-marker'),
+      this.getSortItem (data, 'Par zone drop',  'map-marker'),
+      this.getSortItem (data, 'Par produits',   'cube'),
+      this.getSortItem (data, 'Par prix',       'dollar'),
+      this.getSortItem (data, 'Par poids',      'balance-scale'),
     ];
   }
 
@@ -72,6 +136,47 @@ export default class DispatchBacklog extends React.Component {
     ];
   }
 
+  isFiltered (ticket, data) {
+    if (data.Backlog.Filter === 'Tous') {
+      return false;
+    } else {
+      if (!ticket.Trip || !ticket.Trip.Product) {
+        return true;
+      } else if (data.Backlog.Filter.indexOf ('dring') !== -1) {
+        return ticket.Trip.Product.indexOf ('dring') === -1;
+      } else if (data.Backlog.Filter.indexOf ('urgent') !== -1) {
+        return ticket.Trip.Product !== 'Urgent';
+      }
+      return false;
+    }
+  }
+
+  getTickets (tickets, data) {
+    console.log ('getTickets');
+    const result = [];
+    for (var ticket of tickets) {
+      if (!this.isFiltered (ticket, data)) {
+        result.push (ticket);
+      }
+    }
+    if (data.Backlog.Sort.indexOf ('heure pick') !== -1) {
+      return result.sort (sortTimePick);
+    } else if (data.Backlog.Sort.indexOf ('heure drop') !== -1) {
+      return result.sort (sortTimeDrop);
+    } else if (data.Backlog.Sort.indexOf ('zone pick') !== -1) {
+      return result.sort (sortZonePick);
+    } else if (data.Backlog.Sort.indexOf ('zone drop') !== -1) {
+      return result.sort (sortZoneDrop);
+    } else if (data.Backlog.Sort.indexOf ('produit') !== -1) {
+      return result.sort (sortProduct);
+    } else if (data.Backlog.Sort.indexOf ('prix') !== -1) {
+      return result.sort (sortPrice);
+    } else if (data.Backlog.Sort.indexOf ('poids') !== -1) {
+      return result.sort (sortWeight);
+    }
+    return result;
+  }
+
   renderTicket (ticket, data, index) {
     return (
       <Trip key={index} kind='trip-box' id={ticket.id} ticket={ticket} data={data} {...this.link ()} />
@@ -81,14 +186,15 @@ export default class DispatchBacklog extends React.Component {
   renderTickets (tickets, data) {
     const result = [];
     let index = 0;
-    for (var ticket of tickets) {
+    const sortedTickets = this.getTickets (tickets, data);
+    for (var ticket of sortedTickets) {
       result.push (this.renderTicket (ticket, data, index++));
     }
     return result;
   }
 
   render () {
-    let data = this.read ('data');
+    const data = this.read ('data');
 
     return (
       <Container kind='view-stretch' {...this.link ()} >
