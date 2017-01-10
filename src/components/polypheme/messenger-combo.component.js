ca48932d2c2b638999822565da7135db220cfbd5
Daniel Roux <roux@epsitec.ch>
2017-01-10 18:31:57 +0100

    Add operations 'compact/expand and shift' in roadbook.

--- src/components/polypheme/messenger-combo.component.js
+++ src/components/polypheme/messenger-combo.component.js
@@ -8,13 +8,7 @@ import reducerDragAndDrop from '../polypheme/reducer-drag-and-drop.js';
 
 export default class MessengerCombo extends React.Component {
 
-  reduce (action, ticket) {
-    const id   = ticket.id;
-    const data = this.read ('data');
-    reducerDragAndDrop (data, {
-      type: action,
-      id:   id,
-    });
+  update () {
     if (window.document.mock) {
       for (var c of window.document.toUpdate) {
         c.forceUpdate ();
@@ -22,16 +16,69 @@ export default class MessengerCombo extends React.Component {
     }
   }
 
+  reduce (action, id) {
+    const data = this.read ('data');
+    reducerDragAndDrop (data, {
+      type: action,
+      id:   id,
+    });
+    this.update ();
+  }
+
   swapCompacted () {
     console.log ('MessengerCombo.swapCompacted');
     const roadbook = this.read ('roadbook');
-    this.reduce ('SWAP_ROADBOOK_COMPACTED', roadbook);
+    this.reduce ('SWAP_ROADBOOK_COMPACTED', roadbook.id);
+  }
+
+  swapCompactedAndShift () {
+    console.log ('MessengerCombo.swapCompactedAndShift');
+    const data     = this.read ('data');
+    const roadbook = this.read ('roadbook');
+    const compacted  = roadbook.Compacted  === 'true';
+    if (compacted) {
+      this.shiftToBegin ();
+      this.reduce ('SWAP_ROADBOOK_COMPACTED', data.Roadbooks[0].id);
+    } else {
+      this.shiftToEnd ();
+      this.reduce ('SWAP_ROADBOOK_COMPACTED', data.Roadbooks[data.Roadbooks.length - 1].id);
+    }
+  }
+
+  shiftToBegin () {
+    console.log ('MessengerCombo.shiftToBegin');
+    const data     = this.read ('data');
+    const roadbook = this.read ('roadbook');
+    reducerDragAndDrop (data, {
+      type:        'DROP',
+      fromKind:    'roadbook',
+      fromIds:     [roadbook.id],
+      toId:        data.Roadbooks[0].id,
+      toOwnerId:   data.id,
+      toOwnerKind: 'roadbook',
+    });
+    this.update ();
+  }
+
+  shiftToEnd () {
+    console.log ('MessengerCombo.shiftToEnd');
+    const data     = this.read ('data');
+    const roadbook = this.read ('roadbook');
+    reducerDragAndDrop (data, {
+      type:        'DROP',
+      fromKind:    'roadbook',
+      fromIds:     [roadbook.id],
+      toId:        null,
+      toOwnerId:   data.id,
+      toOwnerKind: 'roadbook',
+    });
+    this.update ();
   }
 
   swapShowHidden () {
     console.log ('MessengerCombo.swapShowHidden');
     const roadbook = this.read ('roadbook');
-    this.reduce ('SWAP_ROADBOOK_SHOWHIDDEN', roadbook);
+    this.reduce ('SWAP_ROADBOOK_SHOWHIDDEN', roadbook.id);
   }
 
   closeCombo () {
@@ -55,6 +102,7 @@ export default class MessengerCombo extends React.Component {
   }
 
   getList () {
+    const data     = this.read ('data');
     const roadbook = this.read ('roadbook');
     const compacted  = roadbook.Compacted  === 'true';
     const showHidden = roadbook.ShowHidden === 'true';
@@ -76,10 +124,33 @@ export default class MessengerCombo extends React.Component {
     list.push (
       {
         text:   compacted ? 'Étendre' : 'Réduire',
-        glyph:  compacted ? 'arrow-right' : 'arrow-left',
+        glyph:  compacted ? 'expand' : 'compress',
         action: () => this.swapCompacted (),
       }
     );
+    if (data.Roadbooks.length > 1) {
+      list.push (
+        {
+          text:   compacted ? 'Étendre et pousser' : 'Réduire et pousser',
+          glyph:  compacted ? 'fast-backward' : 'fast-forward',
+          action: () => this.swapCompactedAndShift (),
+        }
+      );
+      list.push (
+        {
+          text:   'Pousser à gauche',
+          glyph:  'step-backward',
+          action: () => this.shiftToBegin (),
+        }
+      );
+      list.push (
+        {
+          text:   'Pousser à droite',
+          glyph:  'step-forward',
+          action: () => this.shiftToEnd (),
+        }
+      );
+    }
     list.push (
       {
         text:   showHidden ? 'Cacher les livrés' : 'Montrer les livrés',
