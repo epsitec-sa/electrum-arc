9602e32c68b279a201818dafd2b6c33bf966e6b1
Daniel Roux <roux@epsitec.ch>
2017-01-20 16:34:03 +0100

    Create new component CheckButton.

---
+++ src/components/buttons/check-button.component.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import React from 'react';
+import {Button} from '../../all-components.js';
+
+/******************************************************************************/
+
+export default class CheckButton extends React.Component {
+
+  constructor (props) {
+    super (props);
+  }
+
+  render () {
+    const kind    = this.read ('kind');
+    const text    = this.read ('text');
+    const checked = this.read ('checked');
+    const spacing = this.read ('spacing');
+
+    let glyph;
+    if (kind === 'switch') {
+      glyph = (checked === 'true') ? 'toggle-on' : 'toggle-off';  // [ o] [x ]
+    } else if (kind === 'radio') {
+      glyph = (checked === 'true') ? 'stop-circle-o' : 'circle-o';  // o
+    } else {
+      glyph = (checked === 'true') ? 'check-square' : 'square-o';  // [v] [ ]
+    }
+
+    return (
+      <Button glyph={glyph} text={text} border='none' spacing={spacing} {...this.link ()} />
+    );
+  }
+}
+
+/******************************************************************************/
