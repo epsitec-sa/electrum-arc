cc9d1068c167be8c4b16ba1c0c818c35550ed84c
Daniel Roux <roux@epsitec.ch>
2017-02-03 16:25:37 +0100

    Remove dead code in StateManager.

--- src/components/polypheme/state-manager.js
+++ src/components/polypheme/state-manager.js
@@ -7,172 +7,12 @@ function getState () {
   return window.document.stateManager;
 }
 
-function getStateMessengerShowHiddens () {
-  const state = getState ();
-  if (!state.MessengerShowHiddens) {
-    state.MessengerShowHiddens = [];
-  }
-  return state.MessengerShowHiddens;
-}
-
-function getStateMessengerCompacteds () {
-  const state = getState ();
-  if (!state.MessengerCompacteds) {
-    state.MessengerCompacteds = [];
-  }
-  return state.MessengerCompacteds;
-}
-
-function getStateTicketSelecteds () {
-  const state = getState ();
-  if (!state.TicketSelecteds) {
-    state.TicketSelecteds = [];
-  }
-  return state.TicketSelecteds;
-}
-
-function getStateTicketExtendeds () {
-  const state = getState ();
-  if (!state.TicketExtendeds) {
-    state.TicketExtendeds = [];
-  }
-  return state.TicketExtendeds;
-}
-
 function getStateTicketShapes () {
   const state = getState ();
   if (!state.TicketShapes) {
     state.TicketShapes = new Map ();
   }
   return state.TicketShapes;
-}
-
-// ------------------------------------------------------------------------------------------
-
-function isMessengerShowHidden (id) {
-  const state = getStateMessengerShowHiddens ();
-  return state.indexOf (id) !== -1;
-}
-
-function setMessengerShowHidden (id) {
-  const state = getStateMessengerShowHiddens ();
-  const i = state.indexOf (id);
-  if (i === -1) {
-    state.push (id);
-  }
-}
-
-function clearMessengerShowHidden (id) {
-  const state = getStateMessengerShowHiddens ();
-  const i = state.indexOf (id);
-  if (i !== -1) {
-    state.splice (i, 1);
-  }
-}
-
-function putMessengerShowHidden (id, value) {
-  if (value) {
-    return setMessengerShowHidden (id);
-  } else {
-    return clearMessengerShowHidden (id);
-  }
-}
-
-// ------------------------------------------------------------------------------------------
-
-function isMessengerCompacted (id) {
-  const state = getStateMessengerCompacteds ();
-  return state.indexOf (id) !== -1;
-}
-
-function setMessengerCompacted (id) {
-  const state = getStateMessengerCompacteds ();
-  const i = state.indexOf (id);
-  if (i === -1) {
-    state.push (id);
-  }
-  return state;
-}
-
-function clearMessengerCompacted (id) {
-  const state = getStateMessengerCompacteds ();
-  const i = state.indexOf (id);
-  if (i !== -1) {
-    state.splice (i, 1);
-  }
-  return state;
-}
-
-function putMessengerCompacted (id, value) {
-  if (value) {
-    return setMessengerCompacted (id);
-  } else {
-    return clearMessengerCompacted (id);
-  }
-}
-
-// ------------------------------------------------------------------------------------------
-
-function isTicketSelected (id) {
-  const state = getStateTicketSelecteds ();
-  return state.indexOf (id) !== -1;
-}
-
-function setTicketSelected (id) {
-  const state = getStateTicketSelecteds ();
-  const i = state.indexOf (id);
-  if (i === -1) {
-    state.push (id);
-  }
-}
-
-function clearTicketSelected (id) {
-  const state = getStateTicketSelecteds ();
-  const i = state.indexOf (id);
-  if (i !== -1) {
-    state.splice (i, 1);
-  }
-}
-
-function putTicketSelected (id, value) {
-  if (value) {
-    return setTicketSelected (id);
-  } else {
-    return clearTicketSelected (id);
-  }
-}
-
-// ------------------------------------------------------------------------------------------
-
-function isTicketExtended (id) {
-  const state = getStateTicketExtendeds ();
-  return state.indexOf (id) !== -1;
-}
-
-function setTicketExtended (id) {
-  const state = getStateTicketExtendeds ();
-  const i = state.indexOf (id);
-  if (i === -1) {
-    state.push (id);
-  }
-  return state;
-}
-
-function clearTicketExtended (id) {
-  const state = getStateTicketExtendeds ();
-  const i = state.indexOf (id);
-  if (i !== -1) {
-    state.splice (i, 1);
-  }
-  return state;
-}
-
-function putTicketExtended (id, value) {
-  if (value) {
-    return setTicketExtended (id);
-  } else {
-    return clearTicketExtended (id);
-  }
 }
 
 // ------------------------------------------------------------------------------------------
@@ -224,10 +64,6 @@ function setSplitterBacklogWidth (value) {
 // ------------------------------------------------------------------------------------------
 
 module.exports = {
-  isMessengerShowHidden, setMessengerShowHidden, clearMessengerShowHidden, putMessengerShowHidden,
-  isMessengerCompacted, setMessengerCompacted, clearMessengerCompacted, putMessengerCompacted,
-  isTicketSelected, setTicketSelected, clearTicketSelected, putTicketSelected,
-  isTicketExtended, setTicketExtended, clearTicketExtended, putTicketExtended,
   getTicketShape, setTicketShape,
   getSplitterRoadbooksHeight, setSplitterRoadbooksHeight,
   getSplitterBacklogWidth, setSplitterBacklogWidth,
