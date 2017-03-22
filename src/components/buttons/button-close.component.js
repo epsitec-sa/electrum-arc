5df24b500ad6a1f5c37f4911c42d0444804b2920
Daniel Roux <roux@epsitec.ch>
2017-03-22 11:09:28 +0100

    Propagate only properties 'active' and 'selected' in ButtonClose component.

--- src/components/buttons/button-close.component.js
+++ src/components/buttons/button-close.component.js
@@ -15,14 +15,15 @@ export default class ButtonClose extends React.Component {
   render () {
     const {state} = this.props;
     const disabled = Action.isDisabled (state);
-    const kind   = this.read ('kind');
-    const text   = this.read ('text');
-    const active = this.read ('active');
+    const kind     = this.read ('kind');
+    const text     = this.read ('text');
+    const active   = this.read ('active');
+    const selected = this.read ('selected');
 
     return (
       <Container disabled={disabled} kind='row' {...this.link ()}>
-        <Button text={text} kind={kind} {...this.props} {...this.link ()} />
-        <Button glyph='close' kind={kind} active={active} {...this.link ()} />
+        <Button text={text}   kind={kind} active={active} selected={selected} {...this.link ()} />
+        <Button glyph='close' kind={kind} active={active} selected={selected} {...this.link ()} />
       </Container>
     );
   }
